4621c3dfaf9416064327c4678665fcfb10bf33c8
Jan Ossolinski <ossolinski.jan@gmail.com>
2021-04-12 14:34:25 +0000

    Switch database to MongoDB Atlas

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://admin-test:kodilla@cluster0.tert9.mongodb.net/NewWaveDB?retryWrites=true&w=majority', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
